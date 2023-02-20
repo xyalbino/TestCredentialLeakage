ec3b2eec217d565966f3a668179c501069c92008
Eugene <1989095112@qq.com>
2023-02-20 14:10:18 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+# Testing on 2/20/2023, exact match
+
+Brand1 = "accountnow"
+Brand2 = "go2bank"
+Brand3 = "rapidpay"
+Brand4 = "rushcard"
+USERNAME = "admin"
+PASSWORD = "P@ssW0rd"
+HOST = "greendotcorp"
