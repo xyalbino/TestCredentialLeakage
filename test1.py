929de7e30e430cf32eee186ea380bd91b7611b30
Eugene <1989095112@qq.com>
2023-02-20 14:16:56 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,5 @@
 # Testing on 2/20/2023, exact match
 
-Brand1 = "accountnow"
-Brand2 = "go2bank"
-Brand3 = "rapidpay"
-Brand4 = "rushcard"
+Brand1 = "xyalbinobrand"
 USERNAME = "admin"
 PASSWORD = "P@ssW0rd"
-HOST = "greendotcorp"
